423ed0c8ed0db3660724559c3ed912d7d0513791
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-03 08:36:47 -0300

    Involvement API created

---
+++ src/appData.js
@@ -0,0 +1,3 @@
+export const appData = {
+  id: 'Pfhv1GPAtfgdcQ8y9tqr'
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+import { postLike, getLikes, postComment, getCommets } from './involvementApi';
+
+//postComment('bulbasaur', 'Oscar', 'Great pokemon');
+//getCommets('bulbasaur')
