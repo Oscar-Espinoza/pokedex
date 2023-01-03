1d1ac7f05b9877d5b5ebbc6be2d59e967e40d1ef
Oscar-Espinoza <oscardavid2094@gmail.com>
2023-01-03 17:16:53 -0300

    pokeFetch returning array of pokemons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import './style.css';
-import { postLike, getLikes, postComment, getCommets } from './involvementApi';
-import pokeFetch from './modules/pokeFetch'
+import { postLike, getLikes, postComment, getCommets } from './modules/involvementApi';
+import { updateModal } from './modules/popUp';
+import { pokeFetch, createPokeCard } from './modules/pokeFetch'
+
+let pokemonList = []
+document.addEventListener('DOMContentLoaded', async () => {
+  pokemonList = await pokeFetch()
+  console.log(pokemonList);
+});
 
 
-
-document.addEventListener('DOMContentLoaded', pokeFetch());
+document.getElementById('exampleModal').addEventListener('show.bs.modal', (e) => {
+  const modal = e.target
+  const trigger = e.relatedTarget
+  updateModal(modal, trigger)
+})
