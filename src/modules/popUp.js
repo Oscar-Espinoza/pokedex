ce0ea3cfb267abc4d1106e12b00626650bb2d4d8
gijoe2day <gjuliao32@hotmail.com>
2023-01-04 07:45:24 -0500

    Added feature to create comment and working

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,5 +1,15 @@
+import { postComment } from "./involvementApi";
 export const updateModal = (modal, trigger) => {
-  const pokeName = trigger.getAttribute('data-bs-name')
-  const pokeImgUrl = document.getElementById(`${pokeName}-img`).src
-  modal.querySelector('#modal-img').src = pokeImgUrl
-}
+  const pokeName = trigger.getAttribute("data-bs-name");
+  const pokeImgUrl = document.getElementById(`${pokeName}-img`).src;
+  modal.querySelector("#modal-img").src = pokeImgUrl;
+  modal.querySelector("#exampleModalLabel").innerText = pokeName;
+  modal.querySelector("#modal-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userName = document.querySelector("#username").value;
+    const commentBody = document.querySelector("#comment").value;
+    postComment(pokeName, userName, commentBody);
+    document.querySelector("#username").value = "";
+    document.querySelector("#comment").value = "";
+  });
+};
